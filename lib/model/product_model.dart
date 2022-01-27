f3ff40b687e3c500d77ca022ad0f1ba9698ee226
Hugnry-bird <rnjsgudtjr@naver.com>
2022-01-27 22:11:25 +0900

    product class wit factory func

---
+++ lib/model/product_model.dart
@@ -0,0 +1,24 @@
+class Product {
+  int id;
+  int price;
+  String name;
+  String? mainImage;
+  String defaultImage;
+
+  Product(
+      {required this.id,
+      required this.price,
+      required this.name,
+      required this.mainImage,
+      required this.defaultImage});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      price: json['price'],
+      name: json['name'],
+      mainImage: json['main_image'],
+      defaultImage: json['default_image'],
+    );
+  }
+}
