9ad108018d9b90354ea8dc6a021411d85873cbe3
kwonhyeongseok <rnjsgudtjr@naver.com>
2022-03-01 16:53:47 +0900

    barrel 파일 생성

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -1,3 +1,4 @@
 export 'auth_bloc/authentication_bloc.dart';
-export 'myproducts/myproducts_bloc.dart';
+export 'regist_product_bloc/regist_product_bloc.dart';
+export 'myproducts_bloc/myproducts_bloc.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
